aca95fb5e641130bdd074b6b80ff43b4d58d2850
amyjzhu <ajyzhu@gmail.com>
2021-01-29 19:03:12 -0700

    Fix lattice

--- src/main/java/org/checkerframework/checker/dividebyzero/qual/Zero.java
+++ src/main/java/org/checkerframework/checker/dividebyzero/qual/Zero.java
@@ -4,6 +4,6 @@ import org.checkerframework.framework.qual.SubtypeOf;
 import java.lang.annotation.Target;
 import java.lang.annotation.ElementType;
 
-@SubtypeOf({MaybeZero})
+@SubtypeOf({MaybeZero.class})
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
 public @interface Zero {}
